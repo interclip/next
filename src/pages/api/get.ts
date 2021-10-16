e3f2aa812ea336b92884b1342fb874c6561fda21
Filip Troníček <filip.tronicek@seznam.cz>
2021-10-16 22:35:15 +0200

    Add a REST API (#4)
    Co-authored-by: bigint <bigint@users.noreply.github.com>

---
+++ src/pages/api/get.ts
@@ -0,0 +1,56 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { APIResponse } from '../../lib/types';
+import { db } from '../../lib/prisma';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<APIResponse>
+) {
+    const { code: clipCode } = req.query;
+
+    if (!clipCode) {
+        res.status(400).json({
+            status: 'error',
+            result: 'No code provided.'
+        });
+        return;
+    }
+
+    if (typeof clipCode === 'object') {
+        res.status(400).json({
+            status: 'error',
+            result: 'Too many code query params provided. Please only query one code per request.'
+        });
+        return;
+    }
+
+    if (!clipCode.match(new RegExp(/^[A-Za-z0-9]{5}$/))) {
+        res.status(400).json({
+            status: 'error',
+            result: 'The provided code has an invalid format.'
+        });
+    }
+
+    const queriedClip = db.clip.findUnique({
+        where: {
+            code: clipCode,
+        }
+    });
+
+    try {
+        const clipResult = await queriedClip;
+        if (clipResult) {
+            res.status(200).json({ status: 'success', result: clipResult })
+        } else {
+            res.status(404).json({
+                status: 'error',
+                result: 'Clip not found.'
+            });
+        }
+    } catch (e) {
+        res.status(500).json({
+            status: 'error',
+            result: 'An error with the database has occured.'
+        });
+    }
+}
--- src/pages/api/set.ts
+++ src/pages/api/set.ts
@@ -1,17 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import isURL from 'validator/lib/isURL';
+import { APIResponse } from '../../lib/types';
 import { db } from '../../lib/prisma';
 import { dateAddDays } from '../../lib/dates';
 import { getRandomID } from '../../lib/generateID';
 
-type Data = {
-    status: 'error' | 'success',
-    result: any
-}
-
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Data>
+    res: NextApiResponse<APIResponse>
 ) {
     const { url: clipURL } = req.query;
 
@@ -38,21 +34,32 @@ export default async function handler(
         });
     }
 
-    const newClip = db.clip.create({
-        data: {
-            code: getRandomID(5),
-            url: clipURL,
-            expiresAt: dateAddDays(new Date(), 30),
-            createdAt: new Date()
+    const duplicateClip = await db.clip.findFirst({
+        where: {
+            url: clipURL
         }
     });
 
-    try {
-        res.status(200).json({ status: 'success', result: await newClip })
-    } catch (e) {
-        res.status(500).json({
-            status: 'error',
-            result: 'An error with the database has occured.'
+    if (duplicateClip) {
+        res.status(200).json({ status: 'success', result: duplicateClip });
+    } else {
+        const newClip = db.clip.create({
+            data: {
+                code: getRandomID(5),
+                url: clipURL,
+                expiresAt: dateAddDays(new Date(), 30),
+                createdAt: new Date()
+            }
         });
+
+        try {
+            res.status(200).json({ status: 'success', result: await newClip })
+        } catch (e) {
+            res.status(500).json({
+                status: 'error',
+                result: 'An error with the database has occured.'
+            });
+        }
     }
+
 }
