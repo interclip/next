9725ba0be32d540289fce9ec07b7887985246bd7
Filip Troníček <filip@gitpod.io>
2021-10-17 13:07:15 +0000

    Tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {
+    colors: {
+      themeBG: '#157EFB',
+      themeText: '#ffffff',
+      darkBG: '#151515',
+      darkText: '#E4E4E4',
+    },
     extend: {},
   },
   variants: {
