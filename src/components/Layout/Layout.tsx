8cdbada4d522244e4fba7dc2756e3b6c289c24b4
Filip Troníček <filip@gitpod.io>
2021-10-17 13:13:31 +0000

    Remove layout fragment notation

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Layout = (props: any): JSX.Element => {
   return (
-    <>
-      <div className="flex justify-center align-center bg-light-bg text-light-text dark:bg-dark-bg dark:text-dark-text">
-        {props.children}
-      </div>
-    </>
+    <div className="flex justify-center align-center bg-light-bg text-light-text dark:bg-dark-bg dark:text-dark-text">
+      {props.children}
+    </div>
   );
 };
 
