c25c830da5338c4ca9fc45e672e61d5ddb30f234
Filip Troníček <filip@gitpod.io>
2021-10-18 20:33:22 +0000

    Auth navbar support

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,14 +7,16 @@ const Button = ({
   hover_color,
   focus_ring_color,
   url,
+  onClick,
 }: {
   content?: string;
   background_color?: string;
   hover_color?: string;
   focus_ring_color?: string;
   url?: any;
+  onClick?: () => any;
 }) => {
-  return (
+  return url ? (
     <>
       <Link href={`${url}`} passHref={true}>
         <button
@@ -23,6 +25,15 @@ const Button = ({
           <div>{content}</div>
         </button>
       </Link>
+    </>
+  ) : (
+    <>
+      <button
+        onClick={onClick}
+        className={`bg-${background_color} hover:bg-${hover_color} border text-white focus:ring-${focus_ring_color} px-4 py-1.5 rounded-lg font-bold disabled:opacity-50 shadow-sm focus:ring-2 focus:ring-opacity-50 focus:ring-offset-1 outline-none`}
+      >
+        <div>{content}</div>
+      </button>
     </>
   );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Link from 'next/link';
 import NavbarItem from './NavbarItem';
 import { Button } from '../Button';
 import Image from 'next/image';
 import NavbarSection from './NavbarSection';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const Navbar = () => {
+  const { data: session } = useSession();
+
   return (
     <>
       <nav className="bg-white h-16 w-full shadow-lg sticky top-0 z-50 flex justify-center align-center">
@@ -20,18 +22,28 @@ const Navbar = () => {
           </NavbarSection>
           <NavbarSection>
             <NavbarItem url="/" name="Clip" />
-            <NavbarItem url="/dashboard" name="Receive" />
-            <NavbarItem url="/dashboard" name="File" />
+            <NavbarItem url="/receive" name="Receive" />
+            <NavbarItem url="/file" name="File" />
             <NavbarItem url="/about" name="About" />
           </NavbarSection>
           <NavbarSection>
-            <Button
-              content="Login"
-              background_color="light-bg"
-              hover_color="blue-600"
-              focus_ring_color="blue-400"
-              url="/"
-            />
+            {session ? (
+              <Image
+                src="https://avatar.tobi.sh/name.svg?"
+                height={50}
+                width={50}
+                className="rounded-full cursor-pointer"
+                onClick={() => signOut()}
+              />
+            ) : (
+              <Button
+                content="Login"
+                background_color="light-bg"
+                hover_color="blue-600"
+                focus_ring_color="blue-400"
+                onClick={() => signIn()}
+              />
+            )}
           </NavbarSection>
         </div>
       </nav>
