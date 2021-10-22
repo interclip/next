abe6fbd0c6178599eefc10f950abe10303c347ef
Filip Troníček <filip@gitpod.io>
2021-10-22 23:19:29 +0000

    Add clip preview page

---
+++ src/components/shared/QRModal.tsx
@@ -0,0 +1,40 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { useWindowSize } from '@utils/hooks/useWindowSize';
+import QRCode from 'react-qr-code';
+
+const QRModal = (props: {
+  url: string;
+  setQrCodeZoom: React.Dispatch<React.SetStateAction<boolean>>;
+}): JSX.Element => {
+  const [width, height] = useWindowSize();
+
+  const { url, setQrCodeZoom } = props;
+  const size = Math.min(520, width - width * 0.17, height - height * 0.17);
+  return (
+    <motion.main
+      className="text-center"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0 }}
+          transition={{ duration: 0.15 }}
+          key="modal"
+          className="absolute top-0 left-0 w-screen h-screen z-200 backdrop-filter backdrop-blur-sm flex items-center overflow-hidden"
+          onClick={() => {
+            setQrCodeZoom(false);
+          }}
+        >
+          <div className="m-auto bg-white p-4 rounded-2xl shadow-custom">
+            <QRCode value={url} size={size} level="M" />
+          </div>
+        </motion.div>
+      </AnimatePresence>
+    </motion.main>
+  );
+};
+
+export default QRModal;
--- src/pages/[code].tsx
+++ src/pages/[code].tsx
@@ -1,8 +1,53 @@
+import React, { useState } from 'react';
+import { Layout } from '@components/Layout';
 import { db } from '@utils/prisma';
 import { NextApiRequest } from 'next';
+import QRModal from '@components/shared/QRModal';
+import { QRIcon } from '@components/Icons';
+import { Link } from '@components/Text/link';
+import { getLinkPreview } from 'link-preview-js';
+interface OEmbed {
+  url: string;
+  title: string;
+  siteName: string | null;
+  description: string | null;
+  mediaType: string;
+  contentType: string | null;
+  images: string[];
+  videos: {}[];
+  favicons: string[];
+}
 
-const Redirect = () => {
-  return <></>;
+const Redirect = (props: { code: string; url: string; oembed: OEmbed }) => {
+  const [qrCodeZoom, setQrCodeZoom] = useState<boolean>(false);
+  const urlObject = new URL(props.url);
+  const simplifiedURL = `${urlObject.hostname}${urlObject.pathname}`;
+  return (
+    <Layout>
+      <section className="w-full h-full flex flex-col items-center justify-center">
+        <div className="p-4 rounded-2xl mb-8 flex text-black dark:text-white bg-white dark:bg-[#262A2B] shadow-custom">
+          <div className="mr-6">
+            <h2 className="text-4xl mb-2 max-w-[30rem]">
+              {props.oembed.title || props.code}
+            </h2>
+            <h3 className="text-2xl text-gray-400">
+              <Link className="no-underline" href={props.url}>
+                {simplifiedURL}
+              </Link>
+            </h3>
+          </div>
+          <QRIcon
+            onClick={() => {
+              setQrCodeZoom(true);
+            }}
+          />
+          {qrCodeZoom && (
+            <QRModal url={props.url} setQrCodeZoom={setQrCodeZoom} />
+          )}
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
 export async function getServerSideProps({
@@ -11,8 +56,42 @@ export async function getServerSideProps({
   query: NextApiRequest['query'];
 }) {
   const userCode = query.code;
+  const isPreviewPage = userCode.indexOf('+') === userCode.length - 1;
   if (userCode && typeof userCode === 'object') {
     return { notFound: true };
+  }
+
+  if (isPreviewPage) {
+    try {
+      const selectedClip = await db.clip.findUnique({
+        where: { code: userCode.slice(0, -1) },
+      });
+
+      if (!selectedClip) {
+        return { notFound: true };
+      }
+      const additionalDetails = (await getLinkPreview(
+        selectedClip.url,
+      )) as OEmbed;
+      console.log(additionalDetails);
+
+      return {
+        props: {
+          code: selectedClip.code,
+          url: selectedClip.url,
+          oembed: {
+            title: additionalDetails.title || null,
+            description: additionalDetails.description || null,
+            favicons: additionalDetails.favicons,
+          },
+        },
+      };
+    } catch (e) {
+      console.error(e);
+      return {
+        notFound: true,
+      };
+    }
   }
 
   try {
