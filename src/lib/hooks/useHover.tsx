bab07e7b89d3d27f6ff1fcc286fa84249891a807
Filip Troníček <filip@gitpod.io>
2021-10-23 13:58:02 +0000

    Types

--- src/lib/hooks/useHover.tsx
+++ src/lib/hooks/useHover.tsx
@@ -1,24 +1,21 @@
 import { useState, useRef, useEffect } from 'react';
 
 function useHover() {
-  const [value, setValue] = useState(false);
+  const [value, setValue] = useState<boolean>(false);
   const ref = useRef(null);
   const handleMouseOver = () => setValue(true);
   const handleMouseOut = () => setValue(false);
-  useEffect(
-    () => {
-      const node = ref.current;
-      if (node) {
-        node.addEventListener('mouseover', handleMouseOver);
-        node.addEventListener('mouseout', handleMouseOut);
-        return () => {
-          node.removeEventListener('mouseover', handleMouseOver);
-          node.removeEventListener('mouseout', handleMouseOut);
-        };
-      }
-    },
-    [ref.current], // Recall only if ref changes
-  );
+  useEffect(() => {
+    const node = ref.current as HTMLElement | null;
+    if (node) {
+      node.addEventListener('mouseover', handleMouseOver);
+      node.addEventListener('mouseout', handleMouseOut);
+      return () => {
+        node.removeEventListener('mouseover', handleMouseOver);
+        node.removeEventListener('mouseout', handleMouseOut);
+      };
+    }
+  }, [ref.current]);
   return [ref, value];
 }
 
