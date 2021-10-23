fdbe31dfe152df36a44cd2706d1c5e88fc37f556
Filip Troníček <filip.tronicek@seznam.cz>
2021-10-23 18:37:02 +0200

    Add a tag and never use `passHref`
    Co-authored-by: bigint <b1gint@protonmail.com>

--- src/components/Clips/ClipCard.tsx
+++ src/components/Clips/ClipCard.tsx
@@ -8,26 +8,28 @@ const ClipCard = ({ clip }: { clip: Clip }) => {
   const relativeTimeDiff = dayjs().to(dayjs(clip.createdAt));
 
   return (
-    <Link href={`/${clip.code}+`} passHref>
-      <div className="max-w-none sm:max-w-sm bg-white dark:bg-dark-secondary dark:text-dark-text border-2 cursor-pointer border-gray-300 p-6 rounded-md tracking-wide shadow-lg">
-        <>
-          <div className="flex items-center mb-4">
-            <div className="leading-5 sm">
-              <h4 className="text-xl font-semibold text-gray-800 dark:text-dark-text">
-                Code: {clip.code}
-              </h4>
-              <h5 className="font-semibold text-blue-600">
-                Created {relativeTimeDiff}
-              </h5>
+    <Link href={`/${clip.code}+`}>
+      <a href={`/${clip.code}+`}>
+        <div className="max-w-none sm:max-w-sm bg-white dark:bg-dark-secondary dark:text-dark-text border-2 cursor-pointer border-gray-300 p-6 rounded-md tracking-wide shadow-lg">
+          <>
+            <div className="flex items-center mb-4">
+              <div className="leading-5 sm">
+                <h4 className="text-xl font-semibold text-gray-800 dark:text-dark-text">
+                  Code: {clip.code}
+                </h4>
+                <h5 className="font-semibold text-blue-600">
+                  Created {relativeTimeDiff}
+                </h5>
+              </div>
             </div>
-          </div>
-          <div>
-            <span className="italic break-words text-gray-600 dark:text-dark-text">
-              {clip.url}
-            </span>
-          </div>
-        </>
-      </div>
+            <div>
+              <span className="italic break-words text-gray-600 dark:text-dark-text">
+                {clip.url}
+              </span>
+            </div>
+          </>
+        </div>
+      </a>
     </Link>
   );
 };
