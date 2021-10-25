f95d1eaec3326c538f628c2b79f356e46f474169
Filip Troníček <filip@gitpod.io>
2021-10-25 19:03:14 +0000

    Select styles

--- src/components/Settings/Tabs/AppearanceSettings.tsx
+++ src/components/Settings/Tabs/AppearanceSettings.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SettingsCard from '../SettingsCard';
-import Select from 'react-select';
 import { useTheme } from 'next-themes';
 
 const AppearanceSettings = () => {
@@ -15,10 +14,21 @@ const AppearanceSettings = () => {
   return (
     <>
       <SettingsCard title="Color Scheme" onSave={() => {}}>
-        <Select
-          options={themeOptions}
-          onChange={(e) => setTheme(e?.value || 'system')}
-        />
+        <div className="max-w-[50%]">
+          <select
+            className="border-2 border-[#EAEAEA] rounded-xl px-2.5 py-1.5 text-[#333333] dark:text-dark-text :bg-dark-secondary w-full"
+            onChange={(e) => setTheme(e?.target.value || 'system')}
+            defaultValue={theme}
+          >
+            {themeOptions.map((theme) => {
+              return (
+                <option key={theme.value} value={theme.value}>
+                  {theme.label}
+                </option>
+              );
+            })}
+          </select>
+        </div>
       </SettingsCard>
     </>
   );
