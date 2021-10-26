0381da5ce43d910b4564266b6be328697da130e0
Filip Troníček <filip.tronicek@seznam.cz>
2021-10-26 15:57:38 +0000

    A prettier code (funny)

--- src/components/Navbar/NavbarSection.tsx
+++ src/components/Navbar/NavbarSection.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const NavbarSection: React.FC = ({ children }) => {
-  return <div className="flex items-center space-x-4 dark:bg-dark-secondary"> {children}</div>;
+  return (
+    <div className="flex items-center space-x-4 dark:bg-dark-secondary">
+      {children}
+    </div>
+  );
 };
 
 export default NavbarSection;
