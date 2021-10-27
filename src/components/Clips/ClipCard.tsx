d00b7fa22574fec7cca9ca4a3186d98fba325e81
Filip Troníček <filip.tronicek@seznam.cz>
2021-10-27 11:27:21 +0200

    Yogi's return spacing
    Co-authored-by: bigint <b1gint@protonmail.com>

--- src/components/Clips/ClipCard.tsx
+++ src/components/Clips/ClipCard.tsx
@@ -5,6 +5,7 @@ const relativeTime = require('dayjs/plugin/relativeTime');
 dayjs.extend(relativeTime);
 
 const ClipCard = ({ clip }: { clip: ClipWithPreview }) => {
+
   return (
     <Link href={`/new/${clip.code}`} passHref>
       <div className="max-w-none sm:max-w-sm bg-white dark:bg-dark-secondary dark:text-dark-text cursor-pointer h-full p-6 rounded-2xl tracking-wide shadow-lg hover:scale-[0.99]">
