f57100d763bf21835ab6bc9b4b00440860b4567b
Fahad Hossain <fahad@avalonx.io>
2021-10-29 19:29:48 +0600

    create sitemap after build via next-sitemap (#62)
    * add sitemap and postbuild script
    
    * configure next-sitemap #55
    
    * exclude /auth/* from sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,11 @@
+// https://github.com/iamvishnusankar/next-sitemap/blob/master/README.md
+
+module.exports = {
+  siteUrl: process.env.BASE_URL,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  exclude: ['/auth/*', '/500'],
+  outDir: 'public',
+};
