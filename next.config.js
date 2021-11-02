704add8d5fd723f599407483de31ef3a59fa4137
bigint <b1gint@protonmail.com>
2021-11-02 18:11:59 +0530

    feat: disable pwa in dev mode

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   pwa: {
     dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
     register: true,
     skipWaiting: true,
     runtimeCaching: true,
