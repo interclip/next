3ea353d4bf4218701e4934f05d8f199c936f1b6c
Filip Troníček <filip@gitpod.io>
2021-11-05 23:04:29 +0000

    More dark mode

--- src/components/Admin/InfoCard.tsx
+++ src/components/Admin/InfoCard.tsx
@@ -14,14 +14,16 @@ const InfoCard = ({
   return (
     <div className="flex flex-row flex-wrap -mx-2 min-w-8 w-full">
       <div className="mb-4 px-2 w-full">
-        <div className="relative bg-white rounded border w-full">
+        <div className="relative bg-white dark:bg-dark-secondary rounded border w-full">
           <div className="p-4">
             <h3 className="text-lg font-bold">
               <a className="stretched-link" href="#" title="Card 1">
                 {name}
               </a>
             </h3>
-            <p className="block mb-2 text-sm text-gray-600">{description}</p>
+            <p className="block mb-2 text-sm text-gray-600 dark:text-gray-300">
+              {description}
+            </p>
             {value}
             <p>{children}</p>
           </div>
