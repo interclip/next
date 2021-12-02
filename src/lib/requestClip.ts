1ecee947cb44de0d2b6dd5ba8d33c5cef52431e8
Filip Troníček <filip@gitpod.io>
2021-12-02 14:25:31 +0000

    Extending the requestclip file

--- src/lib/requestClip.ts
+++ src/lib/requestClip.ts
@@ -5,15 +5,37 @@ interface ClipResponse extends APIResponse {
   result: Clip;
 }
 
+export class APIError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'APIError';
+  }
+}
+
+/**
+ * Calls the set API to create a new clip from a link
+ * @param url the URL to create the clip from
+ */
 export const requestClip = async (
   url: string,
 ): Promise<ClipResponse | void> => {
   const clipResponse = await fetch(`/api/clip/set?url=${url}`);
-  if (!clipResponse.ok) throw new Error(await clipResponse.text());
+  if (!clipResponse.ok) throw new APIError(await clipResponse.text());
   const clip: ClipResponse = await clipResponse.json();
-  if (clip.status === 'error') {
-    throw new Error(clip.result.toString());
-  }
 
+  return clip;
+};
+
+/**
+ * Calls the get API to get a clip by its corresponding code
+ * @param code the code of the clip
+ */
+export const getClip = async (
+  code: string,
+): Promise<ClipResponse | void | null> => {
+  const clipResponse = await fetch(`/api/clip/get?code=${code}`);
+  if (clipResponse.status === 404) return null;
+  if (!clipResponse.ok) throw new APIError(await clipResponse.text());
+  const clip: ClipResponse = await clipResponse.json();
   return clip;
 };
