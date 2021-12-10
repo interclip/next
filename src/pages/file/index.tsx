44b4754538bd9eac9c5b25dcce86b0b1a25d73bf
Filip Troníček <filip@gitpod.io>
2021-12-10 23:52:17 +0000

    Colors and dark mode options

--- src/pages/file/index.tsx
+++ src/pages/file/index.tsx
@@ -24,7 +24,7 @@ const RemoteOptionsSelect = () => {
     <div className="w-72">
       <Listbox value={selected} onChange={setSelected}>
         <div className="relative mt-1">
-          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 dark:bg-dark-secondary text-dark-text focus-visible:border-indigo-500 sm:text-sm">
             <span className="block truncate">{selected.name}</span>
             <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
               <SelectorIcon
@@ -39,13 +39,17 @@ const RemoteOptionsSelect = () => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white shadow-lg rounded-md max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white shadow-lg rounded-md max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-dark-secondary text-dark-text sm:text-sm">
               {remoteOptions.map((option, optionIdx) => (
                 <Listbox.Option
                   key={optionIdx}
                   className={({ active }) =>
-                    `${active ? 'text-amber-900 bg-amber-100' : 'text-gray-900'}
-                    cursor-default select-none relative py-2 pl-10 pr-4`
+                    `${
+                      active
+                        ? 'text-amber-900 dark:text-light-bg bg-amber-100 dark:bg-dark-bg'
+                        : 'dark:bg-dark-secondary'
+                    }
+                    cursor-default select-none relative py-2 pl-10 pr-4 text-dark-text`
                   }
                   value={option}
                 >
