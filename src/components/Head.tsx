80eaaf74529dc4e46c4bae011cc4ea4234c30206
Filip Troníček <filip.tronicek@seznam.cz>
2022-01-11 18:23:55 +0100

    Remove unused props

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -5,8 +5,6 @@ import React from 'react';
  * A dynamically configurable <head> of the site.
  * @param props -
  * @param props.titlePrefix - a string to be prepended to the site's `<title>` element.
- * @param props.name - a name of an event which is currently counted down to.
- * @param props.date - a formatted difference between the current time and the event.
  * @returns a Head element.
  */
 const PageHead = (props: { titlePrefix?: string }): JSX.Element => {
