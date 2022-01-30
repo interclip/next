0be000216e481b7c527ba768136631b8eb3e1688
Filip Troníček <filip@gitpod.io>
2022-01-30 15:41:51 +0000

    Fix file listbox color in light mode

--- src/pages/file/index.tsx
+++ src/pages/file/index.tsx
@@ -53,7 +53,9 @@ const RemoteOptionsSelect = ({
       <Listbox value={selected} onChange={setSelected}>
         <div className="relative mt-1">
           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left text-dark-text shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 dark:bg-dark-secondary sm:text-sm">
-            <span className="block truncate">{selected.name}</span>
+            <span className="block truncate text-black dark:text-gray-300">
+              {selected.name}
+            </span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <SelectorIcon
                 className="h-5 w-5 text-gray-400"
@@ -86,7 +88,7 @@ const RemoteOptionsSelect = ({
                       <span
                         className={`${
                           selected ? 'font-medium' : 'font-normal'
-                        } block truncate`}
+                        } block truncate text-black dark:text-light-text`}
                       >
                         {option.name}
                       </span>
