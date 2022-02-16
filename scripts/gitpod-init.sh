0719b62e5123796cce9299fbb982869841f017e3
Filip Troníček <filip@gitpod.io>
2022-02-16 20:50:52 +0000

    Remove the `scripts` directory

--- scripts/gitpod-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-yarn install --frozen-lockfile --silent --network-timeout 100000
-cp .env.example .env
-echo "JWT_SECRET=\"$(head -1 <(fold -w 20 <(tr -dc 'a-zA-Z0-9' </dev/urandom)))\"" >>.env
-echo "AUTH_SECRET=\"$(head -1 <(fold -w 20 <(tr -dc 'a-zA-Z0-9' </dev/urandom)))\"" >>.env
-gp await-port 3306
-yarn prisma:migrate
-yarn prisma:seed
