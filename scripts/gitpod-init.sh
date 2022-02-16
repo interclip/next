34ab4fe5f63473da016ce0c1d8f7b8dde2a54175
Filip Troníček <filip@gitpod.io>
2022-02-16 20:23:16 +0000

    Update gitpod config
    Thanks @bigint for the inspiration from Devparty :)

---
+++ scripts/gitpod-init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+yarn install --frozen-lockfile --silent --network-timeout 100000
+cp .env.example .env
+echo "JWT_SECRET=\"$(head -1 <(fold -w 20  <(tr -dc 'a-zA-Z0-9' < /dev/urandom)))\"" >> .env;
+echo "AUTH_SECRET=\"$(head -1 <(fold -w 20  <(tr -dc 'a-zA-Z0-9' < /dev/urandom)))\"" >> .env;
+gp await-port 3306
+yarn prisma:migrate
+yarn prisma:seed
