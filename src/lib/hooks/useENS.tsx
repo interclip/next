be0f1b585c1acb7746e21558c987d50be106bce8
Filip Troníček <filip@gitpod.io>
2022-02-22 12:26:45 +0000

    Don't fail on email addreses

--- src/lib/hooks/useENS.tsx
+++ src/lib/hooks/useENS.tsx
@@ -1,12 +1,13 @@
 import { getDefaultProvider } from '@ethersproject/providers';
 import { useEffect, useState } from 'react';
+import isEthereumAddress from 'validator/lib/isEthereumAddress';
 
 export function useENS(address: string | null | undefined) {
   const [ensName, setENSName] = useState<string | null>();
 
   useEffect(() => {
     async function resolveENS() {
-      if (address) {
+      if (address && isEthereumAddress(address)) {
         const provider = getDefaultProvider();
         const name = await provider.lookupAddress(address);
         if (name) setENSName(name);
