af13e640b149260c0bb0772a5510c7f1a2d24828
Filip Troníček <filip.tronicek@seznam.cz>
2022-03-20 00:21:04 +0100

    Fix paths

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,160 +25,160 @@ class InterclipDocument extends Document<Props> {
           <link href="/icons/logo.svg" rel="icon" />
           <link href="/manifest.json" rel="manifest" />
           <link
-            href="public/pwa/favicon-196.png"
+            href="/pwa/favicon-196.png"
             rel="icon"
             sizes="196x196"
             type="image/png"
           />
 
           <meta
-            content="public/pwa/mstile-icon-128.png"
+            content="/pwa/mstile-icon-128.png"
             name="msapplication-square70x70logo"
           />
           <meta
-            content="public/pwa/mstile-icon-270.png"
+            content="/pwa/mstile-icon-270.png"
             name="msapplication-square150x150logo"
           />
           <meta
-            content="public/pwa/mstile-icon-558.png"
+            content="/pwa/mstile-icon-558.png"
             name="msapplication-square310x310logo"
           />
           <meta
-            content="public/pwa/mstile-icon-558-270.png"
+            content="/pwa/mstile-icon-558-270.png"
             name="msapplication-wide310x150logo"
           />
 
-          <link href="public/pwa/apple-icon-180.png" rel="apple-touch-icon" />
+          <link href="/pwa/apple-icon-180.png" rel="apple-touch-icon" />
 
           <meta content="yes" name="apple-mobile-web-app-capable" />
 
           <link
-            href="public/pwa/apple-splash-dark-2048-2732.jpg"
+            href="/pwa/apple-splash-dark-2048-2732.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-2732-2048.jpg"
+            href="/pwa/apple-splash-dark-2732-2048.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-1668-2388.jpg"
+            href="/pwa/apple-splash-dark-1668-2388.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-2388-1668.jpg"
+            href="/pwa/apple-splash-dark-2388-1668.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-1536-2048.jpg"
+            href="/pwa/apple-splash-dark-1536-2048.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-2048-1536.jpg"
+            href="/pwa/apple-splash-dark-2048-1536.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-1668-2224.jpg"
+            href="/pwa/apple-splash-dark-1668-2224.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-2224-1668.jpg"
+            href="/pwa/apple-splash-dark-2224-1668.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-1620-2160.jpg"
+            href="/pwa/apple-splash-dark-1620-2160.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-2160-1620.jpg"
+            href="/pwa/apple-splash-dark-2160-1620.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-1284-2778.jpg"
+            href="/pwa/apple-splash-dark-1284-2778.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-2778-1284.jpg"
+            href="/pwa/apple-splash-dark-2778-1284.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-1170-2532.jpg"
+            href="/pwa/apple-splash-dark-1170-2532.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-2532-1170.jpg"
+            href="/pwa/apple-splash-dark-2532-1170.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-1125-2436.jpg"
+            href="/pwa/apple-splash-dark-1125-2436.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-2436-1125.jpg"
+            href="/pwa/apple-splash-dark-2436-1125.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-1242-2688.jpg"
+            href="/pwa/apple-splash-dark-1242-2688.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-2688-1242.jpg"
+            href="/pwa/apple-splash-dark-2688-1242.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-828-1792.jpg"
+            href="/pwa/apple-splash-dark-828-1792.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-1792-828.jpg"
+            href="/pwa/apple-splash-dark-1792-828.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-1242-2208.jpg"
+            href="/pwa/apple-splash-dark-1242-2208.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-2208-1242.jpg"
+            href="/pwa/apple-splash-dark-2208-1242.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-750-1334.jpg"
+            href="/pwa/apple-splash-dark-750-1334.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-1334-750.jpg"
+            href="/pwa/apple-splash-dark-1334-750.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-640-1136.jpg"
+            href="/pwa/apple-splash-dark-640-1136.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="public/pwa/apple-splash-dark-1136-640.jpg"
+            href="/pwa/apple-splash-dark-1136-640.jpg"
             media="(prefers-color-scheme: dark) and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
             rel="apple-touch-startup-image"
           />
