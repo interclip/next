9b2a42fbfd0af95380f803f3fcb93a6bdec0a8df
Filip Troníček <filip@gitpod.io>
2022-03-21 10:50:44 +0000

    Dynamically set color theme

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { useTheme } from 'next-themes';
 import React from 'react';
 
 /**
@@ -9,9 +10,15 @@ import React from 'react';
  */
 const PageHead = (props: { titlePrefix?: string }): JSX.Element => {
   const { titlePrefix } = props;
+  const theme = useTheme();
+  console.log(theme);
   return (
     <Head>
       <title>{titlePrefix && `${titlePrefix} |`} Interclip</title>
+      <meta
+        content={theme.resolvedTheme === 'dark' ? '#151515' : '#167efb'}
+        name="theme-color"
+      />
     </Head>
   );
 };
