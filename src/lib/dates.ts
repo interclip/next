6c91f130a616dde3e6d3b583453890d8f31d494c
Filip Troníček <filip@gitpod.io>
2022-10-22 13:56:48 +0000

    Fix JSDoc param

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -1,7 +1,7 @@
 /**
  * Adds an amount of seconds to a Date object
  * @param date a Date object
- * @param seconds a number of seconds to add
+ * @param days a number of seconds to add
  * @returns a new Date object with `seconds` added.
  */
 export function dateAddDays(date: Date, days: number): Date {
